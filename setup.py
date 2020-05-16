a650d048ee38abe837ebd43b6e7a634e2c38b268
Derek Haynes <derek.haynes@gmail.com>
2020-05-16 07:06:46 -0600

    Updating setup.py with project dependencies

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # You can test that the package works and contains needed dependencies by running `tox` from the
     # command line. tox tests the package in an isoloated venv.
     install_requires=[
-        'whisk==0.1.27'
+        'whisk==0.1.27', 'keras>=2.3.1', 'tensorflow-cpu'
     ],
     entry_points={
         'console_scripts': [
