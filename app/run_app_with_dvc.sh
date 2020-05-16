cd59318b90822d0bd19b313db4176263ed111577
Derek Haynes <derek.haynes@gmail.com>
2020-05-16 11:29:00 -0600

    Try .profile instead - applies to the entire project vs just Flask

--- app/run_app_with_dvc.sh
+++
@@ -1,5 +0,0 @@
-# On production the model artifacts won't be present as these reside in DVC, not the Git repo.
-# This script pulls down the train.dvc stage outputs prior to running the app.
-git init # Needed as dvc pull will not work if this isn't a Git repo.
-dvc pull train.dvc
-gunicorn app.main:app
